4fb918f6f3e7930bafbeabaf22d2568430e451a5
101639803 <101639803@student.swin.edu.au>
2018-04-26 17:59:22 +1000

    fix-ai

--- Battleships/src/GameMain.cs
+++ Battleships/src/GameMain.cs
@@ -25,7 +25,7 @@ namespace MyGame
                 
                 //Draw onto the screen
                 RefreshScreen(60);
-                ;;
+                
             }
         }
     }
